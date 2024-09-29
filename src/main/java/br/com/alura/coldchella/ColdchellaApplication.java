7c8fd3bd8e7bec67926864d9a2b6f6cbacd212b5
matheus mozart <mmsnborges@gmail.com>
2024-09-28 23:25:00 -0300

    first commit

---
+++ src/main/java/br/com/alura/coldchella/ColdchellaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.coldchella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdchellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColdchellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/coldchella/ColdchellaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.coldchella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdchellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
